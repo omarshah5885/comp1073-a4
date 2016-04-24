2c2f20e9f638235d6563cf25471e04fc3c509180
omarshah5885 <umarshah0@gmail.com>
2016-04-24 13:34:57 -0400

    click redirection; styled canvas; transferred createJS to app.js doc;

--- scripts/app.js
+++ scripts/app.js
@@ -1,68 +1,119 @@
-// DOM manipulation created by Omar Shah for site, omarshah.azurewebsites.net which is a site about me and my works.
+// CreateJS Banner Ad functionality created by Omar Shah for site, http://omar-comp1073-a4.azurewebsites.net/ which is a site about me and my works.
 
 "use strict";
 
-//iify 
 (function () {
-//conditional statement to display my string variables for their respective nodes on each webpage
-	if (location.pathname.substring(location.pathname.lastIndexOf("/") + 1) == 'index.html') {
 
-//string variables are as follows
-		var aboutMeTitle = document.getElementById("aboutMeTitle");
-		aboutMeTitle.innerHTML = "About Me";
+    // step 1 a) create reference to canvas element 
+    var canvas = document.getElementById("canvas");
 
-		var aboutMe = document.getElementById("aboutMe");
-		aboutMe.innerHTML = "Hello, my name is Omar Shah and I am an ambitious developer that wants to be a professional in the field of marketing with emphasis on digital tools and business intelligence. I am currently enrolled at Georgian College's Interactive Web Design and Development program and am seeking a CO-OP for summer 2016 with a firm that has a strong leadership and work culture.";
-
-		var getintouch = document.getElementById("getintouch");
-		getintouch.innerHTML = "Get In Touch!";
-
-//onlick event created to link to my contact page once getintouch button is pressed
-		var button = document.getElementById("getintouch");
-		button.onclick = function () {
-			location.href = "contact.html";
-		};
-
-	} if (location.pathname.substring(location.pathname.lastIndexOf("/") + 1) == 'projects.html') {
-
-//string variables for my figcaption under each project
-		var project1 = document.getElementById("project1");
-		project1.innerHTML = "This is a website I created for a product before the popularization of the internet. I chose to do it on the Nintendo NES game console. I used no framework for this project and all the layout was performed manually";
-
-		var project2 = document.getElementById("project2");
-		project2.innerHTML = "I created an interactive JavaScript game through a complex set of decision structures. The game is based on randomization and each choice has a different set of outcomes.";
-
-		var project3 = document.getElementById("project3");
-		project3.innerHTML = "A PHP coded assignment which links back to few databases I created using SQL. It is a list of NBA players with their stats and any user can add whoever he likes and get an email sent back to him confirming the record. Every input is validated to prevent datatype errors.";
-	}
-	else if (location.pathname.substring(location.pathname.lastIndexOf("/") + 1) == 'contact.html') {
-		var connect = document.getElementById("connect");
-		connect.innerHTML = "Connect With Me";
-
-		var connectBlurb = document.getElementById("connectBlurb");
-		connectBlurb.innerHTML = "If you're interested with how I can help your organization have a better digital footprint, please feel free to contact me.";
-//second onclick function created to link back to home page once submit button is pressed 
-		var submit = document.getElementById("submit");
-		submit.addEventListener( "click", function (event) {
-		//input variables created in order to be displayed on console;
-			var save_contact = document.getElementById("name");
-			var save_contact2 = document.getElementById("email");
-			var save_contact3 = document.getElementById("phone");
-			var save_contact4 = document.getElementById("comment");
-
-			console.log("Name: " + save_contact.value);
-			console.log("Email: " + save_contact2.value);
-			console.log("Phone: " + save_contact3.value);
-			console.log("Comment: " + save_contact4.value);
-			//test function to prevent submit button functionality 
-			event.preventDefault();
-			// location.href = "index.html";
-		} );
-		
-		
-	}
+    // step 1 b) set the size of canvas
+    var screenWidth = 320;
+    var screenHeight = 100;
+    canvas.setAttribute("width", screenWidth);
+    canvas.setAttribute("height", screenHeight);
 
 
+	// step 2) create a staging area for the animation by referencing the canvas 
+    var stage = new createjs.Stage(canvas);
 
+	// step 5 b) declare every animated text or image as a global variable for accessibility purposes  
+	var projectAd, projectText;
+	var adMoveX = 0.5;
+	var adMoveY = 0.5;
+	var isVisible = true;
+
+	// step 3) start an animation session 
+    function init() {
+        // a) establish a frame rate for animation. 
+        createjs.Ticker.framerate = 60;
+
+        // b) link every frame tick event to the animationloop function. 
+        createjs.Ticker.addEventListener("tick", animationLoop);
+
+		// c) call the drawing function of the bitmap object to be animated
+        drawing();
+    }
+
+	// step 4) create animation function which updates the stage upon every frame tick 
+    function animationLoop() {
+		// move projectAd diagonally back and forth across canvas
+		projectAd.x += adMoveX;
+		projectAd.y += adMoveY;
+
+		if (projectAd.y >= screenHeight) {
+			adMoveY -= 0.5;
+        }
+		if (projectAd.x >= screenWidth) {
+			adMoveX -= 0.5
+			adMoveY += 0.5;
+		}
+		if (projectAd.x <= 0) {
+			adMoveX += 0.5;
+			adMoveY += 0.5;
+		}
+
+		// create a glowing effect on text by dimming and raising transparency 
+
+		if (isVisible) {
+			if (projectText.alpha <= 1) {
+				projectText.alpha *= 0.99;
+			}
+			else {
+				isVisible = false;
+			}
+		} else {
+			if (projectText.alpha <= 0.40) {
+				projectText.alpha *= 1.10;
+			}
+			else {
+				isVisible = true;
+			}
+		}
+
+		// after numerous failed attempts at alpha if statement, I decided to create a glowing effect by the following code: 
+		if (projectText.alpha < 0.40) {
+			projectText.alpha = 1.0;
+		}
+
+
+		// redraws stage object per frame from scratch
+        stage.update();
+    }
+
+    // step 5 a) create a drawing function which provides the image or text object that will be animiated 
+    function drawing() {
+		// create an ad image to display on canvas 
+		projectAd = new createjs.Bitmap("content/images/jsgame.jpg");
+		// scale the image
+		projectAd.scaleX = 0.10;
+		projectAd.scaleY = 0.10;
+		// append image to stage 
+        stage.addChild(projectAd);
+
+		// add a text object on canvas to entice viewers to click 
+		projectText = new createjs.Text("View Now!", "20px Candara", "#fff");
+
+		// change registration mark of text object from top left to direct middle
+        projectText.regX = projectText.getMeasuredWidth() * 0.5;
+        projectText.regY = projectText.getMeasuredHeight() * 0.5;
+
+        // move text to the middle of the canvas
+        projectText.x = screenWidth * 0.5;
+        projectText.y = screenHeight * 0.5;
+
+        // append to the stage
+        stage.addChild(projectText);
+
+        projectText.on("click", function () {
+			location.href = "projects.html";
+        });
+
+
+
+    }
+
+    // step 6) load the animation session on windows screen by calling init function
+    window.onload = init;
 
 })();
